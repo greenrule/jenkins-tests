5d3a3bd33c02fcdea401050a7d00e631d8213517
Marina <sms2004@yandex.ru>
2022-03-18 20:58:22 +0500

    add simple test

---
+++ src/test/java/tests/NegativeTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    void test0() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+    @Test
+    void test2() {
+        assertTrue(false);
+    }
+}
