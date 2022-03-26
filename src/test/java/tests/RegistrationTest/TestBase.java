50a7a57f213cc3b9fabcdc840e75edbf91c594cc
Marina <sms2004@yandex.ru>
2022-03-27 00:15:21 +0500

    add url adress

--- src/test/java/tests/RegistrationTest/TestBase.java
+++ src/test/java/tests/RegistrationTest/TestBase.java
@@ -3,12 +3,9 @@ package tests.RegistrationTest;
 запуск из терминала gradle clean test -Dlogin=user1 -Dpassword=1234 - запускаются все тесты
 gradle clean registration_test -Dlogin=user1 -Dpassword=1234 */
 import com.codeborne.selenide.Configuration;
-import com.sun.source.util.SourcePositions;
 import helpers.Attach;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.sql.SQLOutput;
 
 public class TestBase {
     @BeforeAll
@@ -18,14 +15,12 @@ public class TestBase {
 
         String login = System.getProperty("login", "notRight");
         String password = System.getProperty("password", "123");
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub");
 
         //дженкинс подключается удаленно к хрому, сначала пишем логин/пароль, потом название сайта selenoid.autotests.cloud
-        Configuration.remote = "https://" + login + ":" + password + "@" + System.getProperty("remoteBrowser");
-        //"selenoid.autotests.cloud/wd/hub"
-
+        Configuration.remote = "https://" + login + ":" + password + "@" + url;
 
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 
         //специальная функция, чтобы был доступен просмотр и видео
         DesiredCapabilities capabilities = new DesiredCapabilities();
