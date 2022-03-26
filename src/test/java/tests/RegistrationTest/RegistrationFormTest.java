7d9d00c00a90ba8b8523fe02818588ccf7c6c14c
Marina <sms2004@yandex.ru>
2022-03-26 22:58:19 +0500

    add hw11

--- src/test/java/tests/RegistrationTest/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationTest/RegistrationFormTest.java
@@ -1,45 +1,20 @@
 package tests.RegistrationTest;
-//д.з. 10
-        import com.codeborne.selenide.Configuration;
+//д.з. 10-11
         import com.codeborne.selenide.logevents.SelenideLogger;
-        import helpers.Attach;
         import io.qameta.allure.selenide.AllureSelenide;
-        import org.junit.jupiter.api.AfterAll;
-        import org.junit.jupiter.api.BeforeAll;
+        import org.junit.jupiter.api.Tag;
         import org.junit.jupiter.api.Test;
-        import org.openqa.selenium.remote.DesiredCapabilities;
-
         import static com.codeborne.selenide.Condition.text;
         import static com.codeborne.selenide.Selectors.byText;
         import static com.codeborne.selenide.Selenide.*;
         import static io.qameta.allure.Allure.step;
 
-public class RegistrationFormTest {
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        //дженкинс подключается удаленно к хрому, сначала пишем логин/пароль, потом название сайта selenoid.autotests.cloud
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        //специальная функция, чтобы был доступен просмотр и видео
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @AfterAll
-    static void afterAll() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
-
+public class RegistrationFormTest extends TestBase {
     @Test
+    @Tag("registration")
     void registrationFormTest() {
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         step("Открываем главную страницу", () -> {
             open("/automation-practice-form");
